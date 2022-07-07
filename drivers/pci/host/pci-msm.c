4877f6117ee7f8764f3b77077b59e45f96817cb4
Joel Gómez <thegame455min@gmail.com>
2022-07-07 21:42:52 +0000

    drivers: pci-msm: Guard variables with CONFIG_IPC_LOGGING
    Prevents these warnings:
    
    ../drivers/pci/host/pci-msm.c:6784:15: warning: unused variable 'i' [-Wunused-variable]
            int ret = 0, i;
                         ^
    ../drivers/pci/host/pci-msm.c:6785:7: warning: unused variable 'rc_name' [-Wunused-variable]
            char rc_name[MAX_RC_NAME_LEN];
                 ^
    
    Signed-off-by: Joel Gómez <thegame455min@gmail.com>

--- drivers/pci/host/pci-msm.c
+++ drivers/pci/host/pci-msm.c
@@ -6781,8 +6781,11 @@ static struct platform_driver msm_pcie_driver = {
 
 static int __init pcie_init(void)
 {
-	int ret = 0, i;
+	int ret = 0;
+#ifdef CONFIG_IPC_LOGGING
+	int i;
 	char rc_name[MAX_RC_NAME_LEN];
+#endif
 
 	pr_alert("pcie:%s.\n", __func__);
 
